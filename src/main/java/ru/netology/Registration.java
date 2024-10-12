03c53d2f12d2212363d7e1262237ac3d497adf5d
Evgenia Larina <larsik29@yandex.ru>
2024-10-12 18:44:04 +0300

    Added source code and app-ibank.jar

--- src/main/java/ru/netology/Registration.java
+++ src/main/java/ru/netology/Registration.java
@@ -1,2 +1,54 @@
-package ru.netology;public class Registration {
+package ru.netology;
+
+import com.github.javafaker.Faker;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import lombok.Value;
+
+
+import java.util.Locale;
+
+import static io.restassured.RestAssured.given;
+
+public static class Registration {
+    private Registration() {
+    }
+
+    public static RegistrationDto generateUser(String local, String status) {
+        RegistrationDto user = new RegistrationDto(generateLogin(local), generatePassword(local), status);
+        given() // "дано"
+                .spec(requestSpec) // указываем, какую спецификацию используем
+                .body(user) // передаём в теле объект, который будет преобразован в JSON
+                .when() // "когда"
+                .post("/api/system/users") // на какой путь относительно BaseUri отправляем запрос
+                .then() // "тогда ожидаем"
+                .statusCode(200); // код 200 ОК
+
+        return user;
+    }
+
+    public static RegistrationDto getUser(String local, String status) {
+        RegistrationDto user = new RegistrationDto(generateLogin(local), generatePassword(local), status);
+        return user;
+    }
+
+    public static String generateLogin(String local) {
+        Faker faker = new Faker(new Locale(local));
+        return faker.name().fullName();
+    }
+
+    public static String generatePassword(String local) {
+        Faker faker = new Faker(new Locale(local));
+        return faker.internet().password();
+    }
+
+    @Value
+    public static class RegistrationDto {
+        String login;
+        String password;
+        String status;
+    }
+}
 }
